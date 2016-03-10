ba1df1e91c59f45ea2631e3546bb107a4897e1cd
Huy (Harold) Q. BUI <harold@enclave.vn>
2016-03-10 09:48:06 +0700

    move time util to server side

---
+++ myservice/src/main/java/com/api/common/TimeUtil.java
@@ -0,0 +1,21 @@
+package com.api.common;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
+public class TimeUtil {
+    public String getCurrentTime() {
+        Calendar cal = Calendar.getInstance();
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.ENGLISH);
+        return dateFormat.format(cal.getTime());
+    }
+
+    public Date formatDateTime(String dateTime) throws ParseException {
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.ENGLISH);
+        return formatter.parse(dateTime);
+    }
+}
